d344712b3824e46d2a005d55d1313ab7e61a9185
Prostagma <dlinchaki@mail.ru>
2023-06-20 14:21:36 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace robot_location_detection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.radioButton3);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(328, 82);
+            this.panel1.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Enabled = false;
+            this.panel2.Location = new System.Drawing.Point(157, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(166, 72);
+            this.panel2.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 22);
+            this.textBox1.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 1);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(164, 44);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Выбрать файл";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(3, 3);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(105, 20);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Веб-камера";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(3, 29);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(110, 20);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.Text = "Фотография";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(3, 55);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(69, 20);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.Text = "Видео";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(342, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 82);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.Location = new System.Drawing.Point(439, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(850, 609);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1372, 700);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Линчак ЛР6";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System;
+using System.Threading;
+using System.Windows.Forms;
+using Size = OpenCvSharp.Size;
+
+namespace robot_location_detection
+{
+    public partial class Form1 : Form
+    {
+        bool runVideo;
+        VideoCapture capture;
+        Mat matInput;
+        Thread cameraThread;
+        readonly Size sizeObject = new Size(640, 480);
+        string pathToFile;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            panel2.Enabled = true;
+        }
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            panel2.Enabled = false;
+        }
+        private void DisposeVideo()
+        {
+            pictureBox1.Image = null;
+            if (cameraThread != null && cameraThread.IsAlive) cameraThread.Abort();
+            matInput?.Dispose();
+            capture?.Dispose();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (runVideo)
+            {
+                runVideo = false;
+                DisposeVideo();
+                button1.Text = "Старт";
+            }
+            else
+            {
+                runVideo = true;
+                matInput = new Mat();
+
+                if (radioButton1.Checked)
+                {
+                    capture = new VideoCapture(0)
+                    {
+                        FrameHeight = sizeObject.Height,
+                        FrameWidth = sizeObject.Width,
+                        AutoFocus = true
+                    };
+                }
+                cameraThread = new Thread(new ThreadStart(CaptureCameraCallback));
+                cameraThread.Start();
+                button1.Text = "Стоп";
+            }
+        }
+        private void CaptureCameraCallback()
+        {
+            while (runVideo)
+            {
+                matInput = radioButton1.Checked ? capture.RetrieveMat() : new Mat(pathToFile).Resize(sizeObject);
+
+
+                Invoke(new Action(() =>
+                {
+                    pictureBox1.Image = BitmapConverter.ToBitmap(matInput);
+                    GC.Collect();
+                    GC.WaitForPendingFinalizers();
+                }));
+            }
+        }
+    }
+}
