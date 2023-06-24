4455e5e08be2a8d6595accad669080a0fafc0ace
Prostagma <dlinchaki@mail.ru>
2023-06-24 16:05:46 +0300

    добавлен алгоритм для вычичления угла поворота ar-метки

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -37,9 +37,20 @@
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -79,6 +90,7 @@
             this.button2.TabIndex = 8;
             this.button2.Text = "Выбрать файл";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // radioButton1
             // 
@@ -134,22 +146,122 @@
             this.pictureBox1.TabIndex = 9;
             this.pictureBox1.TabStop = false;
             // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.checkBox1);
+            this.panel3.Controls.Add(this.checkBox2);
+            this.panel3.Enabled = false;
+            this.panel3.Location = new System.Drawing.Point(12, 100);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(421, 97);
+            this.panel3.TabIndex = 20;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(135, 16);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Координаты метки :";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(4, 3);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(111, 20);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "Поиск меток";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(4, 29);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(235, 20);
+            this.checkBox2.TabIndex = 13;
+            this.checkBox2.Text = "Отображение найденных меток";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 214);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 230);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(232, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(232, 230);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 16);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "label5";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(16, 268);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(417, 353);
+            this.pictureBox2.TabIndex = 25;
+            this.pictureBox2.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1372, 700);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel3);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Линчак ЛР6";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -164,6 +276,15 @@
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,6 +1,8 @@
 ﻿using OpenCvSharp;
+using OpenCvSharp.Aruco;
 using OpenCvSharp.Extensions;
 using System;
+using System.IO;
 using System.Threading;
 using System.Windows.Forms;
 using Size = OpenCvSharp.Size;
@@ -11,21 +13,19 @@ namespace robot_location_detection
     {
         bool runVideo;
         VideoCapture capture;
+        Dictionary markers;
         Mat matInput;
         Thread cameraThread;
         readonly Size sizeObject = new Size(640, 480);
         string pathToFile;
+        int[] ids;
+        bool showMarks, searchMarks;
+        string stringForLabel = "Координаты метки: ";
+        Point3d[] center;
+        readonly int markerLength = 300;
         public Form1()
         {
             InitializeComponent();
-        }
-        private void radioButton3_CheckedChanged(object sender, EventArgs e)
-        {
-            panel2.Enabled = true;
-        }
-        private void radioButton1_CheckedChanged(object sender, EventArgs e)
-        {
-            panel2.Enabled = false;
         }
         private void DisposeVideo()
         {
@@ -34,17 +34,52 @@ namespace robot_location_detection
             matInput?.Dispose();
             capture?.Dispose();
         }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DisposeVideo();
+        }
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            panel2.Enabled = false;
+        }
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            panel2.Enabled = true;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog()
+            {
+                Multiselect = false
+            };
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                var tempPath = file.FileName;
+                if (File.Exists(tempPath))
+                {
+                    var ext = Path.GetExtension(tempPath);
+                    if (ext == ".png" || ext == ".jpg" || ext == ".jpeg")
+                    {
+                        pathToFile = tempPath;
+                        textBox1.Text = pathToFile;
+                    }
+                }
+            }
+            file.Dispose();
+        }
         private void button1_Click(object sender, EventArgs e)
         {
             if (runVideo)
             {
                 runVideo = false;
+                panel3.Enabled = false;
                 DisposeVideo();
                 button1.Text = "Старт";
             }
             else
             {
                 runVideo = true;
+                panel3.Enabled = true;
                 matInput = new Mat();
 
                 if (radioButton1.Checked)
@@ -61,20 +96,88 @@ namespace robot_location_detection
                 button1.Text = "Стоп";
             }
         }
+
         private void CaptureCameraCallback()
         {
             while (runVideo)
             {
                 matInput = radioButton1.Checked ? capture.RetrieveMat() : new Mat(pathToFile).Resize(sizeObject);
-
-
+                if (searchMarks)
+                {
+                    SearchAndShowMarks(markers, ref matInput, out ids, out center, showMarks);
+                }
                 Invoke(new Action(() =>
                 {
+                    label4.Text = stringForLabel;
                     pictureBox1.Image = BitmapConverter.ToBitmap(matInput);
                     GC.Collect();
                     GC.WaitForPendingFinalizers();
                 }));
             }
+        }
+
+        private void SearchAndShowMarks(Dictionary marksDict, ref Mat inputMat, out int[] ids, out Point3d[] centers, bool drawDetectedMarks)
+        {
+            Point2f[][] corners;
+            var param = new DetectorParameters();
+            CvAruco.DetectMarkers(inputMat, marksDict, out corners, out ids, param, out _);
+            centers = new Point3d[corners.Length];
+
+            for (short i = 0; i < centers.Length; i++)
+            {
+                for (byte j = 0; j < 4; j++)
+                {
+                    centers[i].X += (int)corners[i][j].X;
+                    centers[i].Y += (int)corners[i][j].Y;
+                }
+                centers[i].X /= 4;
+                centers[i].Y /= 4;
+                centers[i].Z = markerLength - (int)Point2f.Distance(corners[i][0], corners[i][1]);
+            }
+
+
+            if (drawDetectedMarks && corners.Length > 0)
+            {
+                label1.Text = corners[0][0].ToString();
+                label2.Text = corners[0][1].ToString();
+                float turnY = corners[0][0].Y - corners[0][1].Y;
+                float turnX = corners[0][0].X - corners[0][1].X;
+
+                label5.Text = (Math.Atan2(turnY, turnX)*180/Math.PI).ToString();
+                label3.Text = Math.Abs(turnY) > 5 ? (turnY < 0 ? "->" : "<-") : "||";
+
+                CvAruco.DrawDetectedDiamonds(inputMat, corners);
+                for (short i = 0; i < centers.Length; i++)
+                {
+                    inputMat.Circle((int)centers[i].X, (int)centers[i].Y, 2, Scalar.Red, 2);
+                    stringForLabel = $"Координаты метки:    X:{(int)centers[i].X} Y:{(int)centers[i].Y} Z:{(int)centers[i].Z}";
+                    inputMat.PutText($"X:{(int)centers[i].X} Y:{(int)centers[i].Y} Z:{(int)centers[i].Z}", new Point((int)centers[i].X, (int)centers[i].Y), HersheyFonts.HersheySimplex, 0.5, Scalar.Red);
+                }
+            }
+        }
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            showMarks = checkBox2.Checked;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            markers = CvAruco.GetPredefinedDictionary(PredefinedDictionaryName.Dict7X7_50);
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            searchMarks = checkBox1.Checked;
         }
     }
 }
